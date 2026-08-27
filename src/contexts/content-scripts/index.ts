221903d75c6d5eebfc06142c4bdb088980f0e8e6
heppokofrontend <heppokofrontend@gmail.com>
2026-08-28 01:52:36 +0900

    Extract createGetFileSize into utils/get-file-size
    - Move the self-contained fetch-based file-size/type factory out of the
      content-scripts entry point into utils, matching the DI pattern already
      used by createSetImageData / createZoomAndScrollInit

--- src/contexts/content-scripts/index.ts
+++ src/contexts/content-scripts/index.ts
@@ -11,57 +11,12 @@ import {
   convertedImgToSVGMap,
   convertedSvgMap,
   convertSVGToImg,
+  createGetFileSize,
   createSetImageData,
   createZoomAndScrollInit,
   defaultState,
   getImageData,
 } from '@/contexts/content-scripts/utils';
-
-const createGetFileSize = ({
-  setImageData,
-}: {
-  setImageData: ReturnType<typeof createSetImageData>;
-}) => {
-  return (image: HTMLImageElement) => {
-    return new Promise<void>((done) => {
-      const isSVG = image.src.startsWith('data:image/svg+xml');
-
-      if (isSVG) {
-        const size = new Blob([image.src]).size;
-
-        setImageData(image, {
-          fileSize: size ? `${size} byte` : chrome.i18n.getMessage('error_fileSize'),
-          fileType: 'image/svg+xml (in HTML)',
-        });
-
-        done();
-        return;
-      }
-
-      const { protocol } = new URL(image.src);
-
-      fetch(image.src.replace(protocol, location.protocol), { method: 'HEAD' })
-        .then(({ headers }) => {
-          const size = headers.get('Content-Length');
-          const type = headers.get('Content-Type');
-
-          setImageData(image, {
-            fileSize: size ? `${size} byte` : chrome.i18n.getMessage('error_fileSize'),
-            fileType: type ?? chrome.i18n.getMessage('error_fileType'),
-          });
-        })
-        .catch(() => {
-          setImageData(image, {
-            fileSize: chrome.i18n.getMessage('error_fileSize'),
-            fileType: chrome.i18n.getMessage('error_fileType'),
-          });
-        })
-        .finally(() => {
-          done();
-        });
-    });
-  };
-};
 
 const { imageViewer, dialog, canvas, spaceElement } = CONTENT_UI;
 
